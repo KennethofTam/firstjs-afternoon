fd27a9aff869578d4d85d5fc6c83cd12b6887949
Kenneth Tam <ktam@purdue.edu>
2016-06-01 15:11:59 -0400

    Created functions to complete the form + created dl element in a function

--- js/app.js
+++ js/app.js
@@ -1,28 +1,106 @@
 $(document).foundation()
 
-var myForm = document.querySelector('form');
-myForm.onsubmit = function(ev) {
-  ev.preventDefault();
-  var details = document.querySelector('div.details');
-  var firstName = this.firstName.value;
-  var hairColor = this.hairColor.value;
-  var age = this.age.value;
-  var birthplace = this.birthplace.value;
-  var colorDiv = '<div style="height: 100px; width: 100px; background-color: ' + hairColor + '"></div>'
-  details.innerHTML += ' \
+/*
+function addValuesToDetails() {
+  this
+}
+*/
+
+/** buildColorDiv
+  *   args:color
+  *   returns: the div as a string
+  */
+var buildColorDiv = function(color) {
+    return '<div style="height: 100px; width: 100px; background-color: ' + color + '"></div>';
+  };
+
+/* An object is essentially a collection of properties
+  ex. obj.propName
+  var n = 7; var s = "hello"; var a = []; var f = function() {};
+  */
+
+  /*
+  var davey = {
+    'firstName': 'Davey', //property name is a string
+    'lastName': 'Strus'
+  };
+
+  To retrieve values:
+  davey['firstName']
+
+  To modify values:
+  davey['lastName'] = 'Jones'
+  */
+
+/**  buildList
+  *   args: firstName, age, hairColorDiv, birthplace
+  */
+var buildList = function(listValues) {
+  var dl = document.createElement('dl');
+  dl.style.borderColor = 'red';
+  dl.innerHTML = buildListItem('First Name', listValues.firstName) +
+  buildListItem('Hair Color', listValues.hairColor) +
+  buildListItem('Age', listValues.age) +
+  buildListItem('Birthplace', listValues.birthplace);
+
+  return dl;
+
+  /*
+  return ' \
   <dl> \
-  <dt>First Name</dt>\
-  <dd>' + firstName  + '<dd> \
-    \
-  <dt>Hair Color</dt> \
-    <dd>' + colorDiv  + '</dd> \
-    \
-  <dt>Age</dt> \
-    <dd>' + age + '</dd> \
-    \
-  <dt>Birthplace</dt> \
-  <dd>' + birthplace + '</dd> \
-    \
-  </dl>';
+    ' + buildListItem('First Name', listValues.firstName) + '\
+    ' + buildListItem('Hair Color', listValues.hairColor) + '\
+    ' + buildListItem('Age', listValues.age) + '\
+    ' + buildListItem('Birthplace', listValues.birthplace) + '\
+  </dl>';*/
 
 };
+
+var buildListItem = function(term, definition) {
+  var li = ' \
+  <li> \
+    <dt>'+ term +'</dt> \
+    <dd>' + definition + '</dd> \
+  </li>';
+  return li;
+}
+
+var addValuesToDetails = function(ev) {
+  ev.preventDefault();
+  var details = document.querySelector('div.details');
+  var hairColor = this.hairColor.value;
+  var listValues = {
+      firstName: this.firstName.value,
+      age: this.age.value,
+      birthplace: this.birthplace.value,
+      hairColor: buildColorDiv(hairColor)
+    };
+
+    //var firstName = this.firstName.value;
+    //var age = this.age.value;
+    //var birthplace = this.birthplace.value;
+    //var colorDiv = buildColorDiv(hairColor);
+
+    //details.innerHTML += buildList(listValues);
+details.appendChild(buildList(listValues));
+    /*
+    details.innerHTML += ' \
+    <dl> \
+    <dt>First Name</dt>\
+    <dd>' + firstName  + '<dd> \
+      \
+    <dt>Hair Color</dt> \
+      <dd>' + colorDiv  + '</dd> \
+      \
+    <dt>Age</dt> \
+      <dd>' + age + '</dd> \
+      \
+    <dt>Birthplace</dt> \
+    <dd>' + birthplace + '</dd> \
+      \
+    </dl>'; */
+  };
+
+
+var myForm = document.querySelector('form');
+myForm.onsubmit = addValuesToDetails;
